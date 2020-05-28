9b81b315b689fe4a44ba9f206c926e87c6cb007a
Lena Bolz <bolz.lena@gmail.com>
2020-05-28 19:06:20 -0400

    Link js to html.

--- dothis/tasks/static/tasks/js/task_list.js
+++ dothis/tasks/static/tasks/js/task_list.js
@@ -1,9 +1,8 @@
-// need to modify to be use table
 function tableDropDown() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
+  var row = document.getElementById("row-task-detail");
+  if (row.style.display === "none") {
+    row.style.display = "block";
   } else {
-    x.style.display = "none";
+    row.style.display = "none";
   }
 }
