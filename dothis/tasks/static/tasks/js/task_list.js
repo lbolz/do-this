7ab6512998ac0239f9f105d17ecc885e8060af64
Lena Bolz <bolz.lena@gmail.com>
2020-05-28 17:59:23 -0400

    Start working on css

---
+++ dothis/tasks/static/tasks/js/task_list.js
@@ -0,0 +1,9 @@
+// need to modify to be use table
+function tableDropDown() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
