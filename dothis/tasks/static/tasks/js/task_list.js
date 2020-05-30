333749c5e5cf20705d1494d329b582484e9c0b24
Lena Bolz <bolz.lena@gmail.com>
2020-05-30 13:06:32 -0400

    Rewrote task list html.

--- dothis/tasks/static/tasks/js/task_list.js
+++
@@ -1,8 +0,0 @@
-function bodyDropDown() {
-  var body = document.getElementsByClassName("task-body");
-  if (body.style.display[0] == "none") {
-    body.style.display = "block";
-  } else {
-    body.style.display = "none";
-  }
-}
