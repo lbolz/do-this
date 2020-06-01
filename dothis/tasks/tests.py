8d6836d682687b2c5a5739865200bc802fed4196
Lena Bolz <bolz.lena@gmail.com>
2020-06-01 09:17:06 -0400

    Add tests.

--- dothis/tasks/tests.py
+++ dothis/tasks/tests.py
@@ -1,7 +1,15 @@
+from django.urls import resolve
 from django.test import TestCase
 from tasks.models import Task
 
+from tasks.views import task_list
+
 # Create your tests here.
+class TaskListPageTest(TestCase):
+	def test_root_url_resolves_to_task_list_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, task_list)
+
 class TaskModelTest(TestCase):
 	def test_string_representation(self):
 		task = Task(title="Task title")
